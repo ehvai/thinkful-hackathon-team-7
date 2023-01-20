ce05fd4962b2bbce270c5114ea62a485c7b1ef83
Mikhail <mikhailj.lajom@gmail.com>
2023-01-20 15:16:58 -0700

    completed signup, signup form

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:3000";
+	process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
@@ -23,26 +23,26 @@ headers.append("Content-Type", "application/json");
  *  If the response is not in the 200 - 399 range the promise is rejected.
  */
 async function fetchJson(url, options, onCancel) {
-  try {
-    const response = await fetch(url, options);
+	try {
+		const response = await fetch(url, options);
 
-    if (response.status === 204) {
-      return null;
-    }
+		if (response.status === 204) {
+			return null;
+		}
 
-    const payload = await response.json();
+		const payload = await response.json();
 
-    if (payload.error) {
-      return Promise.reject({ message: payload.error });
-    }
-    return payload.data;
-  } catch (error) {
-    if (error.name !== "AbortError") {
-      console.error(error.stack);
-      throw error;
-    }
-    return Promise.resolve(onCancel);
-  }
+		if (payload.error) {
+			return Promise.reject({ message: payload.error });
+		}
+		return payload.data;
+	} catch (error) {
+		if (error.name !== "AbortError") {
+			console.error(error.stack);
+			throw error;
+		}
+		return Promise.resolve(onCancel);
+	}
 }
 
 /**
@@ -51,11 +51,52 @@ async function fetchJson(url, options, onCancel) {
  *  a promise that resolves to a possibly empty array of reservation saved in the database.
  */
 
-// api call
-export async function loginCredentials(){
+/**
+ * Send a Post request to login
+ * @return {Promise<[user login]>}
+ *  a promise
+ */
+export async function loginCredentials(user, signal) {
+	const url = `${API_BASE_URL}/user/login`;
+	const options = {
+		method: "POST",
+		headers,
+		body: JSON.stringify({ data: user }),
+		signal,
+	};
 
+	return await fetchJson(url, options, {});
 }
 
-// api call for creating account
+/**
+ * Send a Post request to login
+ * @return {Promise<[user login]>}
+ *  a promise
+ */
+export async function logoutCredentials(signal) {
+	const url = `${API_BASE_URL}/user/logout`;
+	const options = {
+		method: "GET",
+		headers,
+		signal,
+	};
 
+	return await fetchJson(url, options, {});
+}
 
+/**
+ * Send a Post request to create a user's login
+ * @return {Promise<[new user]>}
+ *  a promise with new user
+ */
+export async function createUser(user, signal) {
+	const url = `${API_BASE_URL}/user`;
+	const options = {
+		method: "POST",
+		headers,
+		body: JSON.stringify({ data: user }),
+		signal,
+	};
+
+	return await fetchJson(url, options, {});
+}
